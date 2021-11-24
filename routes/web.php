ccdcc1cdca63e3091208b00365fa18088cb988a6
mahogony <mahogony@immerda.ch>
2021-11-24 11:15:54 -0500

    Add route to switch language

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+/**
+ * This route will change the local of the current user and save selection in the session
+ * After this it will redirect back to the origin url
+ */
+Route::get('/language/{locale}', function ($locale) {
+    app()->setLocale($locale);
+    session()->put('locale', $locale);
+    // go back
+    return redirect()->back();
 });
