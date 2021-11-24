ee2808f149fc0aa9959f22967fbc0b152c589d01
mahogony <mahogony@immerda.ch>
2021-11-24 11:20:50 -0500

    Add translations for home route

---
+++ resources/lang/de/home.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    "shiftplanInfo" => "Schichtpäne zum Koordinieren von HelferInnen für Veranstaltungen",
+    "Shiftplan" => "Schichtplan",
+    "deleteInfo" => "Pläne werden 30 Tage nach dem letzten Eintrag gelöscht",
+    "createPlan" => "Neuen Plan erstellen",
+    "AGPLlogo" => "AGPL logo",
+];
---
+++ resources/lang/en/home.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    "shiftplanInfo" => "Shift planning to coordinate volunteers and events",
+    "Shiftplan" => "Shift Planner",
+    "deleteInfo" => "Plans will be deleted 30 after the last entry",
+    "createPlan" => "Create a new Plan",
+    "AGPLlogo" => "AGPL logo",
+];
