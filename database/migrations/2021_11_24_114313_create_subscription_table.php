773dd11cc22b8376d4c96f5d251b50594285741f
mahogony <mahogony@immerda.ch>
2021-11-24 06:48:12 -0500

    Add subscription migration

---
+++ database/migrations/2021_11_24_114313_create_subscription_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubscriptionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     * Add or modify subscription table
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('subscription')) {
+            Schema::create('subscription', function (Blueprint $table) {
+                $table->id();
+                // the typo is in the origin repo
+                $table->char('name', 60);
+                $table->string('phone', 20);
+                $table->string('email', 200);
+                $table->string('comment',500);
+                $table->foreignIdFor(Shift::class)
+                    ->constrained()
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+                $table->timestamps();
+            });
+        }
+
+        // add timestamps to existing table
+        if (!Schema::hasColumn('subscription', 'created_at')) {
+            Schema::table('subscription', function (Blueprint $table) {
+                $table->timestamps();
+            });
+        }
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     * Drop subscription table
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('subscription');
+    }
+}
