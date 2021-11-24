f509594c38ee66b87d5b5e5b31d0b1adc4fb3834
mahogony <mahogony@immerda.ch>
2021-11-24 11:14:31 -0500

    Add language_switcher.blade.php partial and compose it

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // compose our language switcher with the current and all available languages
+        view()->composer('partials.language_switcher', function ($view) {
+            $view->with('current_locale', app()->getLocale());
+            $view->with('available_locales', config('app.available_locales'));
+        });
     }
 }
