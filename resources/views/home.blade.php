9f4f6eaccd306c733bc60d913ef2fbe741fe4c4f
mahogony <mahogony@immerda.ch>
2021-11-24 11:20:14 -0500

    Add home route and some basic theme

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+@extends('layout.app')
+@section('body')
+    <h2>{{ __('home.Shiftplan') }}</h2>
+
+    <p>{{ __('home.shiftplanInfo') }}</p>
+
+    <p>
+        <a href="{{URL::to('plan/add')}}">{{__('home.createPlan')}}</a><br>
+    </p>
+
+    <p>
+        <i><?php __('home.deleteInfo') ?></i><br>
+    </p>
+
+    <div style="font-size:small; float:right;">
+        immerda.ch - <a href=
+                        "https://code.immerda.ch/immerda/apps/schichtplan">src</a><br>
+    </div><br>
+
+    <div style="position:absolute;bottom:0;right:0;">
+        <img src="{{URL::to('/assets/images/agplv3-88x31.png')}}" alt="{{__('home.AGPLlogo')}}">
+    </div>
+
+@endsection
