0a6f0b1bf88639f12424b4999cfa9684ab3db995
mahogony <mahogony@immerda.ch>
2021-12-03 09:07:59 -0500

    Configure routes for home and plan actions

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+/**
+ * BC: old url for create a plan
+ * redirect to plan.create
+ */
+Route::get('/plan/add', function () {
+    return Redirect::route('plan.create');
+});
+
+/**
+ * Plan resource controller
+ */
+Route::resource('plan', \App\Http\Controllers\PlanController::class)->only([
+    'create', 'store', 'update', 'destroy',
+]);
+
+/**
+ * Shift resource controller
+ * Hint: just for auth users
+ */
+Route::resource('plan.shift', \App\Http\Controllers\ShiftController::class)->only([
+    'index', 'create', 'store', 'update', 'destroy',
+])->middleware('auth');
 
 /**
  * This route will change the local of the current user and save selection in the session
