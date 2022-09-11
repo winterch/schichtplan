9242abee2cc9c1ebfb3f8e85b6f0791a907a91db
mahogony <mahogony@immerda.ch>
2022-09-11 06:38:26 -0400

    Fix error with js call to not existing element

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,19 +9,22 @@ function addEvents() {
         time_24hr: true,
     });
 
-    document.getElementById('start').addEventListener("change",
-        (function () {
-            const e1 = new Date(document.getElementById('start').value);
-            const e2 = new Date(e1);
-            e2.setHours(e2.getHours() + 1);
-            document.getElementById('end').flatpickr({
-                defaultDate: e2,
-                minDate: e1,
-                enableTime: true,
-                time_24hr: true,
-            });
-        })
-    );
+    const el = document.getElementById('start');
+    if(el !== null) {
+        el.addEventListener("change",
+            (function () {
+                const e1 = new Date(document.getElementById('start').value);
+                const e2 = new Date(e1);
+                e2.setHours(e2.getHours() + 1);
+                document.getElementById('end').flatpickr({
+                    defaultDate: e2,
+                    minDate: e1,
+                    enableTime: true,
+                    time_24hr: true,
+                });
+            })
+        );
+    }
 }
 
 document.addEventListener('DOMContentLoaded', addEvents);
