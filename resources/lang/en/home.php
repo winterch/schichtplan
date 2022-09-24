29294124c90aeef9f27a8a8050902fa172aa78d4
mh <mh@immerda.ch>
2022-09-24 16:20:06 +0200

    fix some langs

--- resources/lang/en/home.php
+++ resources/lang/en/home.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    "shiftplanInfo" => "Shift planning to coordinate volunteers and events",
+    "shiftplanInfo" => "Shift planning to coordinate volunteers and events, partys or festivals. Create shifts and tasks, where volunteers can sign up.",
     "Shiftplan" => "Shift Planner",
-    "deleteInfo" => "Plans will be deleted 30 after the last entry",
-    "createPlan" => "Create a new Plan",
+    "deleteInfo" => "Plans will be deleted 30 days after the last entry",
+    "createPlan" => "Create a new plan",
     "AGPLlogo" => "AGPL logo",
     "copyleft" => "copyleft www.immerda.ch - <a href= 'https://code.immerda.ch/immerda/apps/schichtplan'>Source</a> - <a href='https://www.gnu.org/licenses/agpl-3.0.en.html'>AGPL License</a>"
 ];
