1675b96821e9d488075486c51b7317ba4e406a3c
mahogony <mahogony@immerda.ch>
2023-12-01 19:23:14 +0000

    Resolve "Allow line breaks in shift plan description"

--- resources/views/partials/plan_title.blade.php
+++ resources/views/partials/plan_title.blade.php
@@ -1,6 +1,6 @@
 <h1 class="text-3xl mb-2">{{ $plan->title }}</h1>
 <br>
-<div class="text-lg italic">{{ $plan->description }}</div>
+<div class="text-lg italic">{!! nl2br(e($plan->description)) !!}</div>
 @if (!empty($plan->contact))
   <br>
   <div class="text-lg">{{ __('plan.responsible') }}: {{ $plan->contact }}</div>
