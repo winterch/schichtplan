e4a8e52d2602f699dbad5e95076f4079f7818f41
Christian Winter <christian-winter@mailbox.org>
2025-08-13 19:57:13 +0200

    Fix flash banner position

--- resources/views/partials/flash.blade.php
+++ resources/views/partials/flash.blade.php
@@ -1,11 +1,11 @@
 @if(Session::has('fail'))
-    <div class="sm:m-6 sm:py-6 w-full bg-red-50 border border-red-500 p-4 my-2 rounded">
+    <div class="sm:my-6 sm:py-6 w-full bg-red-50 border border-red-500 p-4 my-2 rounded">
         {{Session::get('fail')}}
     </div>
 @endif
 
 @if(Session::has('info'))
-    <div class="sm:m-6 sm:py-6 w-full bg-green-50 border p-4 my-2 rounded text-xl">
+    <div class="sm:my-6 sm:py-6 w-full bg-green-50 border p-4 my-2 rounded text-xl">
         {{Session::get('info')}}
     </div>
 @endif
